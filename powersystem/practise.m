e3a542e06219ff28299aa68716d5f54e895bc62c
LJC <651293791@qq.com>
2019-06-23 12:01:22 +0800

    6.23

--- powersystem/practise.m
+++ powersystem/practise.m
@@ -4,11 +4,11 @@ mpc = loadcase('case39');
 % mpc.bus(4,:) =[];
 % mpc.branch(4,:) = [];
 % mpc.branch([5,6,7,8],:) = [];
-N = 5;
+N = 1;
 a = [];
 for i =1:N
-    mpc.bus(28,PD)=normrnd(206,206*0.2);
-%     mpc.bus(4,PD)= 500;
+%     mpc.bus(28,PD)=normrnd(206,206*0.2);%功率变化模型
+    mpc.bus(4,PD)= 500;
     [MVAbase, bus, gen, branch, success,et] = runpf(mpc);
     a(i) = sum(branch(:,PF)+branch(:,PT));
   
